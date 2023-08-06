2a9abb020e60a6451b2d2eb898f0ef7134ea4c7b
jbmitchell02 <jbm8efn@virginia.edu>
2023-08-05 21:22:24 -0400

    Renamed variables for consistency with README

--- arb.py
+++ arb.py
@@ -20,42 +20,42 @@ def arbitrage_calc(apiKey, markets='all'):
 
 def h2h_arb(games):
     for df in games:
-        odds1 = df.iloc[:, 1].max()
-        odds1_index = df.iloc[:, 1].idxmax()
-        odds2 = df.iloc[:, 2].max()
-        odds2_index = df.iloc[:, 2].idxmax()
-        top = odds1 * odds2 - odds1 - odds2
-        bottom = odds1 + odds2
+        v1 = df.iloc[:, 1].max()
+        v1_index = df.iloc[:, 1].idxmax()
+        v2 = df.iloc[:, 2].max()
+        v2_index = df.iloc[:, 2].idxmax()
+        top = v1 * v2 - v1 - v2
+        bottom = v1 + v2
         if top > bottom:
-            returns = (top/bottom)-1
+            R = (top/bottom)-1
             team1 = df.columns[1]
             team2 = df.columns[2]
-            book1 = df.iloc[odds1_index, 0]
-            book2 = df.iloc[odds2_index, 0]
-            bet_prop = odds1/odds2
-            #db.add_h2h_opp('baseball_mlb', team1, team2, book1, book2, odds1, odds2, returns, bet_prop)
-            print(f'H2H ({team1}, {team2}): {round(returns*100, 3)}%')
-            print(f'    {book1}: {odds1} - {team1}')
-            print(f'    {book2}: {odds2} - {team2}')
+            book1 = df.iloc[v1_index, 0]
+            book2 = df.iloc[v2_index, 0]
+            bet_prop = v1/v2
+            #db.add_h2h_opp('baseball_mlb', team1, team2, book1, book2, v1, v2, R, bet_prop)
+            print(f'H2H ({team1}, {team2}): {round(R*100, 3)}%')
+            print(f'    {book1}: {v1} - {team1}')
+            print(f'    {book2}: {v2} - {team2}')
 
 def spreads_arb(games):
     for df in games:
         grouped_df = df.groupby('spread')
         for spread, group in grouped_df:
-            odds1 = group.iloc[:, 1].max()
-            odds1_index = group.iloc[:, 1].idxmax()
-            odds2 = group.iloc[:, 2].max()
-            odds2_index = group.iloc[:, 2].idxmax()
-            top = odds1 * odds2 - odds1 - odds2
-            bottom = odds1 + odds2
+            v1 = group.iloc[:, 1].max()
+            v1_index = group.iloc[:, 1].idxmax()
+            v2 = group.iloc[:, 2].max()
+            v2_index = group.iloc[:, 2].idxmax()
+            top = v1 * v2 - v1 - v2
+            bottom = v1 + v2
             if top > bottom:
-                returns = (top/bottom)-1
+                R = (top/bottom)-1
                 team1 = group.columns[1]
                 team2 = group.columns[2]
-                book1 = group.iloc[odds1_index, 0]
-                book2 = group.iloc[odds2_index, 0]
-                bet_prop = odds1/odds2
-                #db.add_spreads_opp('baseball_mlb', team1, team2, book1, book2, spread, odds1, odds2, returns, bet_prop)
-                print(f'Spread {spread} ({team1}, {team2}): {round(returns*100, 3)}%')
-                print(f'    {book1}: {odds1} - {team1}')
-                print(f'    {book2}: {odds2} - {team2}')
+                book1 = group.iloc[v1_index, 0]
+                book2 = group.iloc[v2_index, 0]
+                bet_prop = v1/v2
+                #db.add_spreads_opp('baseball_mlb', team1, team2, book1, book2, spread, v1, v2, R, bet_prop)
+                print(f'Spread {spread} ({team1}, {team2}): {round(R*100, 3)}%')
+                print(f'    {book1}: {v1} - {team1}')
+                print(f'    {book2}: {v2} - {team2}')
