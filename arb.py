d4473d873edc30dc2c58afb8a11f22545a25f5bc
jbmitchell02 <jbm8efn@virginia.edu>
2023-08-06 20:26:41 -0400

    Cleaned up code and added functionality for totals

--- arb.py
+++ arb.py
@@ -2,66 +2,87 @@ import pandas as pd
 import odds
 import db
 
-def arbitrage_calc(apiKey, markets='all', bookmakers='all'):
-    if markets == 'all':
-        h2h_games = odds.get_mlb_h2h_odds(apiKey, bookmakers)
-        spreads_games = odds.get_mlb_spreads_odds(apiKey, bookmakers)
-        h2h_arb(h2h_games)
-        spreads_arb(spreads_games)
-        return h2h_games + spreads_games
-    elif markets == 'h2h':
-        h2h_games = odds.get_mlb_h2h_odds(apiKey, bookmakers)
-        h2h_arb(h2h_games)
-        return h2h_games
-    elif markets == 'spreads':
-        spreads_games = odds.get_mlb_spreads_odds(apiKey, bookmakers)
-        spreads_arb(spreads_games)
-        return spreads_games
-    else:
-        print('Invalid market. Please choose from: all, h2h, spreads')
-        return None
-
+def arbitrage_calc(apiKey, markets=['all'], bookmakers='all'):
+    if 'all' in markets:
+        h2h = odds.get_mlb_h2h_odds(apiKey, bookmakers)
+        spreads = odds.get_mlb_spreads_odds(apiKey, bookmakers)
+        totals = odds.get_mlb_totals_odds(apiKey, bookmakers)
+        h2h_arb(h2h)
+        spreads_arb(spreads)
+        totals_arb(totals)
+        return
+    if 'h2h' in markets:
+        h2h = odds.get_mlb_h2h_odds(apiKey, bookmakers)
+        h2h_arb(h2h)
+    if 'spreads' in markets:
+        spreads = odds.get_mlb_spreads_odds(apiKey, bookmakers)
+        spreads_arb(spreads)
+    if 'totals' in markets:
+        totals = odds.get_mlb_totals_odds(apiKey, bookmakers)
+        totals_arb(totals)
+    
+def calc_returns(v1, v2):
+    top = v1 * v2 - v1 - v2
+    bottom = v1 + v2
+    return top/bottom
 
 def h2h_arb(games):
-    for df in games:
-        v1 = df.iloc[:, 1].max()
-        v1_index = df.iloc[:, 1].idxmax()
-        v2 = df.iloc[:, 2].max()
-        v2_index = df.iloc[:, 2].idxmax()
-        top = v1 * v2 - v1 - v2
-        bottom = v1 + v2
-        if top > bottom:
-            R = (top/bottom)-1
-            team1 = df.columns[1]
-            team2 = df.columns[2]
-            book1 = df.iloc[v1_index, 0]
-            book2 = df.iloc[v2_index, 0]
+    for game, odds in games.items():
+        v1 = odds.iloc[:, 1].max()
+        v1_index = odds.iloc[:, 1].idxmax()
+        v2 = odds.iloc[:, 2].max()
+        v2_index = odds.iloc[:, 2].idxmax()
+        R = calc_returns(v1, v2)
+        if R > -0.01:
+            team1 = game[0]
+            team2 = game[1]
+            book1 = odds.iloc[v1_index, 0]
+            book2 = odds.iloc[v2_index, 0]
             bet_prop = v1/v2
             #db.add_h2h_opp('baseball_mlb', team1, team2, book1, book2, v1, v2, R, bet_prop)
             print(f'H2H ({team1}, {team2}): {round(R*100, 3)}%')
-            print(f'    {book1}: {v1} - {team1}')
-            print(f'    {book2}: {v2} - {team2}')
-            print(f'    b2={bet_prop}b1')
+            print(f'    {book1}: {v1}, {team1}, {round(1/bet_prop, 3)}*b_2')
+            print(f'    {book2}: {v2}, {team2}, {round(bet_prop, 3)}*b_1')
+            print()
 
 def spreads_arb(games):
-    for df in games:
-        grouped_df = df.groupby('spread')
-        for spread, group in grouped_df:
+    for game, odds in games.items():
+        grouped_odds = odds.groupby('spread')
+        for spread, group in grouped_odds:
             v1 = group.iloc[:, 1].max()
             v1_index = group.iloc[:, 1].idxmax()
             v2 = group.iloc[:, 2].max()
             v2_index = group.iloc[:, 2].idxmax()
-            top = v1 * v2 - v1 - v2
-            bottom = v1 + v2
-            if top > bottom:
-                R = (top/bottom)-1
-                team1 = group.columns[1]
-                team2 = group.columns[2]
+            R = calc_returns(v1, v2)
+            if R > -0.01:
+                team1 = game[0]
+                team2 = game[1]
                 book1 = group.iloc[v1_index, 0]
                 book2 = group.iloc[v2_index, 0]
                 bet_prop = v1/v2
                 #db.add_spreads_opp('baseball_mlb', team1, team2, book1, book2, spread, v1, v2, R, bet_prop)
                 print(f'Spread {spread} ({team1}, {team2}): {round(R*100, 3)}%')
-                print(f'    {book1}: {v1} - {team1}')
-                print(f'    {book2}: {v2} - {team2}')
-                print(f'    b2={bet_prop}b1')
+                print(f'    {book1}: {v1}, {team1}, {round(1/bet_prop, 3)}*b_2')
+                print(f'    {book2}: {v2}, {team2}, {round(bet_prop, 3)}*b_1')
+                print()
+
+def totals_arb(games):
+    for game, odds in games.items():
+        grouped_odds = odds.groupby('total')
+        for total, group in grouped_odds:
+            v1 = group.iloc[:, 1].max()
+            v1_index = group.iloc[:, 1].idxmax()
+            v2 = group.iloc[:, 2].max()
+            v2_index = group.iloc[:, 2].idxmax()
+            R = calc_returns(v1, v2)
+            if R > -0.01:
+                team1 = game[0]
+                team2 = game[1]
+                book1 = group.iloc[v1_index, 0]
+                book2 = group.iloc[v2_index, 0]
+                bet_prop = v1/v2
+                #db.add_totals_opp('baseball_mlb', team1, team2, book1, book2, total, v1, v2, R, bet_prop)
+                print(f'Total {total} ({team1}, {team2}): {round(R*100, 3)}%')
+                print(f'    {book1}: {v1}, {team1}, {round(1/bet_prop, 3)}*b_2')
+                print(f'    {book2}: {v2}, {team2}, {round(bet_prop, 3)}*b_1')
+                print()
--- odds.py
+++ odds.py
@@ -1,9 +1,9 @@
 import requests
 import pandas as pd
 
-def get_mlb_odds(apiKey, regions, markets, oddsFormat, bookmakers='all'):
+def get_odds(sport, apiKey, regions, markets, oddsFormat='decimal', bookmakers='all'):
     response = requests.get(
-        'https://api.the-odds-api.com/v4/sports/baseball_mlb/odds',
+        f'https://api.the-odds-api.com/v4/sports/{sport}/odds',
         params={
             'apiKey': apiKey,
             'regions': regions, # us | us2 | uk | au | eu
@@ -14,28 +14,48 @@ def get_mlb_odds(apiKey, regions, markets, oddsFormat, bookmakers='all'):
     return response
 
 def get_mlb_h2h_odds(apiKey, bookmakers='all'):
-    response = get_mlb_odds(apiKey, 'us', 'h2h', 'decimal', bookmakers)
-    games = []
+    response = get_odds('baseball_mlb', apiKey, 'us', 'h2h', 'decimal', bookmakers)
+    games = {}
     for game in response:
-        odds = {'bookmakers': [], game['home_team']: [], game['away_team']: []}
+        home_team = game['home_team']
+        away_team = game['away_team']
+        odds = {'bookmakers': [], home_team: [], away_team: []}
         for book in game['bookmakers']:
             odds['bookmakers'].append(book['key'])
             odds[book['markets'][0]['outcomes'][0]['name']].append(book['markets'][0]['outcomes'][0]['price'])
             odds[book['markets'][0]['outcomes'][1]['name']].append(book['markets'][0]['outcomes'][1]['price'])
         df = pd.DataFrame(odds)
-        games.append(df)
+        games[(home_team, away_team)] = df
     return games
 
 def get_mlb_spreads_odds(apiKey, bookmakers='all'):
-    response = get_mlb_odds(apiKey, 'us', 'spreads', 'decimal', bookmakers)
-    games = []
+    response = get_odds('baseball_mlb', apiKey, 'us', 'spreads', 'decimal', bookmakers)
+    games = {}
     for game in response:
-        odds = {'bookmakers': [], game['home_team']: [], game['away_team']: [], 'spread': []}
+        home_team = game['home_team']
+        away_team = game['away_team']
+        odds = {'bookmakers': [], home_team: [], away_team: [], 'spread': []}
         for book in game['bookmakers']:
             odds['bookmakers'].append(book['key'])
             odds[book['markets'][0]['outcomes'][0]['name']].append(book['markets'][0]['outcomes'][0]['price'])
             odds[book['markets'][0]['outcomes'][1]['name']].append(book['markets'][0]['outcomes'][1]['price'])
             odds['spread'].append(abs(book['markets'][0]['outcomes'][0]['point']))
         df = pd.DataFrame(odds)
-        games.append(df)
+        games[(home_team, away_team)] = df
+    return games
+
+def get_mlb_totals_odds(apiKey, bookmakers='all'):
+    response = get_odds('baseball_mlb', apiKey, 'us', 'totals', 'decimal', bookmakers)
+    games = {}
+    for game in response:
+        home_team = game['home_team']
+        away_team = game['away_team']
+        odds = {'bookmakers': [], 'Over': [], 'Under': [], 'total': []}
+        for book in game['bookmakers']:
+            odds['bookmakers'].append(book['key'])
+            odds[book['markets'][0]['outcomes'][0]['name']].append(book['markets'][0]['outcomes'][0]['price'])
+            odds[book['markets'][0]['outcomes'][1]['name']].append(book['markets'][0]['outcomes'][1]['price'])
+            odds['total'].append(book['markets'][0]['outcomes'][0]['point'])
+        df = pd.DataFrame(odds)
+        games[(home_team, away_team)] = df
     return games
